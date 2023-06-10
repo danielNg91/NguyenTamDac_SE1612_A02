18191c10c166705a598ff9bd49388836a238da03
danielNG <dacnguyen9101@gmail.com>
2023-06-10 19:31:50 +0700

    feat: participate controller

---
+++ Api/Models/Request/CreateParticipate.cs
@@ -0,0 +1,24 @@
+﻿using Api.Mappings;
+using BusinessObjects;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Models;
+
+public class CreateParticipate : IMapTo<ParticipatingProject>
+{
+
+    [Required]
+    public int CompanyProjectID { get; set; }
+
+    [Required]
+    public int EmployeeID { get; set; }
+
+    [Required]
+    public DateTime StartDate { get; set; }
+
+    [Required]
+    public DateTime EndDate { get; set; }
+
+    [Required]
+    public ProjectRole ProjectRole { get; set; }
+}
---
+++ Api/Models/Request/UpdateParticipate.cs
@@ -0,0 +1,13 @@
+﻿using Api.Mappings;
+using BusinessObjects;
+
+namespace Api.Models;
+
+public class UpdateParticipate : IMapTo<ParticipatingProject>
+{
+    public int? CompanyProjectID { get; set; }
+    public int? EmployeeID { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public ProjectRole? ProjectRole { get; set; }
+}
