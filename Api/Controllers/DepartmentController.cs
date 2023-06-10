ed921158a555b80bcd984971a9e6d64459be1700
danielNG <dacnguyen9101@gmail.com>
2023-06-10 19:36:57 +0700

    feat: dept controller

---
+++ Api/Controllers/DepartmentController.cs
@@ -0,0 +1,33 @@
+﻿using Api.Models;
+using Api.Models;
+using Application.Exceptions;
+using BusinessObjects;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+
+namespace Api.Controllers;
+
+[Route("api/v1/participates")]
+public class ParticipateController : BaseController
+{
+    private readonly IRepository<ParticipatingProject> _parRepo;
+
+    public ParticipateController(IRepository<ParticipatingProject> parRepo)
+    {
+        _parRepo = parRepo;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetPars()
+    {
+        return Ok(await _parRepo.ToListAsync());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreatePar([FromBody] CreateParticipate req)
+    {
+        var entity = Mapper.Map(req, new ParticipatingProject());
+        await _parRepo.CreateAsync(entity);
+        return StatusCode(StatusCodes.Status201Created);
+    }
+}
---
+++ Api/Models/Request/CreateDept.cs
@@ -0,0 +1,14 @@
+﻿using Api.Mappings;
+using BusinessObjects;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Models;
+
+public class CreateDept : IMapTo<Department>
+{
+    [Required]
+    public string DepartmentName { get; set; }
+
+    [Required]
+    public string DepartmentDescription { get; set; }
+}
